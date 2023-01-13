e33774c3e3a29f5d90f9c5943b58d184b38d1140
Miguel Penha <miguel.penha1@gmail.com>
2023-01-13 06:56:03 -0300

    Teste

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = api => {
   api.cache(true)
-  console.log(process.env.PATH_ENV_FILE)
+  
   return {
     presets: ['babel-preset-expo'],
     plugins: [
