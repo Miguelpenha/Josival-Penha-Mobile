ddd35d79ee4e19e9b7428006784a3d458fbb4255
Miguel Penha <miguel.penha1@gmail.com>
2023-02-16 08:22:56 -0300

    Update theme.ts

--- theme.ts
+++ theme.ts
@@ -1,12 +1,12 @@
 import { ITheme } from './types'
 
 export const theme: ITheme = {
-    color: '#fa6565',
-    primary: '#FF0022',
-    secondary: '#8C030E',
-    secondaryColor: '#BF0413',
-    backgroundColor: '#200004',
-    backgroundColorSecondary: '#590212'
+    color: '#71cdef',
+    primary: '#00afef',
+    secondary: '#007daa',
+    secondaryColor: '#0093c9',
+    backgroundColor: '#00131a',
+    backgroundColorSecondary: '#003346'
 }
 
 export default theme
