2e7945fbafe9ebcf17089d6422966652a2059fab
Miguel Penha <miguel.penha1@gmail.com>
2023-04-03 01:07:41 -0300

    Update style.ts

--- pages/Home/style.ts
+++ pages/Home/style.ts
@@ -3,8 +3,9 @@ import Animated from 'react-native-reanimated'
 import { RFPercentage } from 'react-native-responsive-fontsize'
 
 export const Title = styled(Animated.Text)`
-    margin-top: 18%;
+    margin-top: 15%;
     font-weight: bold;
+    margin-bottom: 8%;
     align-self: center;
     font-size: ${RFPercentage(4.5)}px;
     color: ${props => props.theme.primary};
