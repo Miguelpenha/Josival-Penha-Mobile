125b2c26b325ab0502b5fce1f16c6fd4c3e3850e
Miguel Penha <miguel.penha1@gmail.com>
2023-10-06 01:21:12 -0300

    Teste

--- components/icons/IconTeacher/style.ts
+++ components/icons/IconTeacher/style.ts
@@ -3,5 +3,5 @@ import { FontAwesome5 } from '@expo/vector-icons'
 
 export const Icon = styled(FontAwesome5)`
     margin-right: 8%;
-    color: ${props => props.theme.primary};
+    color: ${props => props.theme.secondaryColor};
 `
