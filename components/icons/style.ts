53c9a1adfbf92fd497bcab0dc6e7f9ea15ae166b
Miguel Penha <miguel.penha1@gmail.com>
2023-10-06 01:22:03 -0300

    Update style.ts

--- components/icons/style.ts
+++ components/icons/style.ts
@@ -5,5 +5,5 @@ export const Icon = styled(Svg)`
     width: 32px;
     height: 32px;
     margin-right: 7%;
-    fill: ${props => props.theme.primary};
+    fill: ${props => props.theme.secondaryColor};
 `
