18a2413e6217f6e498a8a205bd191d2b3e23b4a8
Miguel Penha <miguel.penha1@gmail.com>
2023-10-19 18:28:04 -0300

    Update InitialStack.tsx

--- routes/InitialStack.tsx
+++ routes/InitialStack.tsx
@@ -13,23 +13,19 @@ interface IProps {
 const InitialStack: FC<IProps> = ({ initialRoute }) => {
     const { Navigator, Screen } = createStackNavigator<INavigation>()
 
-    if (initialRoute) {
-        return (
-            <Navigator
-                initialRouteName={initialRoute || 'Home'}
-                screenOptions={{
-                    headerShown: false
-                }}
-            >
-                <Screen name="Home" component={Home}/>
-                <Screen name="Sign" component={Sign}/>
-                <Screen name="Login" component={Login}/>
-                <Screen name="NotConnected" component={NotConnected}/>
-            </Navigator>
-        )
-    } else {
-        return null
-    }
+    return (
+        <Navigator
+            initialRouteName={initialRoute || 'Home'}
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Screen name="Home" component={Home}/>
+            <Screen name="Sign" component={Sign}/>
+            <Screen name="Login" component={Login}/>
+            <Screen name="NotConnected" component={NotConnected}/>
+        </Navigator>
+    )
 }
 
 export default InitialStack
