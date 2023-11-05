b1bbdd9b72174b545636c34b4066d14c81c8daba
Miguel Penha <miguel.penha1@gmail.com>
2023-11-05 00:33:18 -0300

    Teste

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -4,6 +4,7 @@ export declare global {
         EXPO_PUBLIC_API_URL: string
         EXPO_PUBLIC_API_KEY: string
         NODE_ENV: 'development' | 'production'
+        EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID: string
         EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID: string
         EXPO_PUBLIC_GOOGLE_EXPO_CLIENT_ID: string
         EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID: string
--- pages/Sign/useGoogle/useAuthRequest.ts
+++ pages/Sign/useGoogle/useAuthRequest.ts
@@ -3,6 +3,7 @@ import * as Google from 'expo-auth-session/providers/google'
 function useAuthRequest() {
     const [request, response, promptAsync] = Google.useAuthRequest({
         webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+        iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
         expoClientId: process.env.EXPO_PUBLIC_GOOGLE_EXPO_CLIENT_ID,
         androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID
     })
